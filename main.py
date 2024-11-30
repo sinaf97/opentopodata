69b5a34b030e4cd9913a0614d0f217f801fb48d2
Sina Farahani <sfarahani15@gmail.com>
2024-11-30 19:34:15 +0330

    initial commit 2
    (cherry picked from commit 8d9b3a01e88df6e51404dc3790b1ce2959025ff7)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import tempfile
 from collections import defaultdict
-
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import numpy as np
@@ -25,13 +24,16 @@ class Payload(BaseModel):
 
 
 # Directory containing the .zip files
-DATA_DIR = os.environ.get("DATA_DIR", "/data")
+DATA_DIR = os.environ.get("DATA_DIR", "/datasets")
+MIN_N, MAX_N = map(int, os.environ.get("N_RANGE", "25-40").split("-"))
+MIN_E, MAX_E = map(int, os.environ.get("E_RANGE", "43-64").split("-"))
 
 # Dictionary to hold elevation data from all loaded files
 elevation_data = {}
 
 
 def load_hgt_files(data_dir):
+    data_dir = os.path.join(DATA_DIR, data_dir)
     """Load all HGT files from the specified directory into memory."""
     global elevation_data
 
@@ -41,7 +43,8 @@ def load_hgt_files(data_dir):
             if file_name.endswith('.zip'):
                 N = int(file_name[1:3])
                 E = int(file_name[5:7])
-                if not (25 < N < 40 and 43 < E < 64):
+
+                if not (MIN_N < N < MAX_N and MIN_E < E < MAX_E):
                     continue
 
                 zip_path = os.path.join(data_dir, file_name)
@@ -66,22 +69,6 @@ def load_hgt_files(data_dir):
                                 print(f"Loaded {extracted_file} with shape {data.shape}")
 
 
-def get_elevation(lat, lon):
-    """Get the elevation at the specified latitude and longitude using nearest neighbor search."""
-    N, E = int(lat), int(lon)
-    data, transform = elevation_data[(N, E)]
-    # Calculate row and column indices based on the transform
-    col, row = ~transform * (lon, lat)
-    row, col = int(row), int(col)
-
-    # Check if the indices are within the bounds of the data
-    if 0 <= row < data.shape[0] and 0 <= col < data.shape[1]:
-        # Direct lookup
-        return data[row, col]
-
-    raise HTTPException(status_code=404, detail="Elevation data not found")
-
-
 def batch_elevation(coordinates):
     grouped_coordinates = defaultdict(list)
     for coord in coordinates:
@@ -93,6 +80,8 @@ def batch_elevation(coordinates):
         latitudes = np.array([float(coord[0]) for coord in points])
         longitudes = np.array([float(coord[1]) for coord in points])
 
+        if file not in elevation_data:
+            raise HTTPException(status_code=422, detail=f"The N{file[0]} E0{file[1]} elevation tile is missing.")
         data, transform = elevation_data[file]
         col, row = ~transform * (longitudes, latitudes)
         row = np.floor(row).astype(int)
@@ -117,16 +106,16 @@ def batch_elevation(coordinates):
 @app.on_event("startup")
 async def startup_event():
     """Load HGT files into memory on server startup."""
-    load_hgt_files(DATA_DIR)
+    load_hgt_files(os.environ.get("DATASET_FOLDER_NAME", "sews-data"))
 
 
 @app.get("/v1/sews-dataset")
 async def elevation_get(locations: str):
     """Get elevations for up to 1,000,000 coordinates."""
     coordinates = [
-        list(map(lambda x: float(x), i.split(",")))
+        list(map(float, i.split(",")))
         for i in locations.split("|")]
-    if len(coordinates) > 10000000:
+    if len(coordinates) > int(float(os.environ.get("MAX_POINTS_COUNT", 10000000))):
         raise HTTPException(status_code=400, detail="Too many coordinates (limit is 1,000,000)")
 
     return {"results": batch_elevation(coordinates)}
@@ -136,9 +125,9 @@ async def elevation_get(locations: str):
 async def elevation_post(payload: Payload):
     """Get elevations for up to 1,000,000 coordinates."""
     coordinates = [
-        list(map(lambda x: float(x), i.split(",")))
+        list(map(float, i.split(",")))
         for i in payload.locations.split("|")]
-    if len(coordinates) > 10000000:
+    if len(coordinates) > int(float(os.environ.get("MAX_POINTS_COUNT", 10000000))):
         raise HTTPException(status_code=400, detail="Too many coordinates (limit is 1,000,000)")
 
     return {"results": batch_elevation(coordinates)}
